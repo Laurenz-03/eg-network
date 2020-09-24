fd50b8afd242521d051063ecf0b842703b150873
Laurenz-03 <laurenzruoff2003@gmail.com>
2020-09-24 19:37:59 +0200

    commit

--- main.py
+++ main.py
@@ -7,17 +7,29 @@ app = Flask(__name__)
 def landingPage():
     return render_template('pages/landingpage.html', isLandingPage = True)
 
+@app.route('/login')
+def login():
+    return render_template('pages/login.html')
 @app.route('/impressum')
 def impressum():
-    return render_template('pages/impressum.html', isLandingPage = False)
-    
+    return render_template('pages/impressum.html')
+
 # Alle Seiten im Mitgliederbereich
 @app.route('/mgb')
 def mgb():
-    return render_template('pages/mgb.html', isLandingPage = False)
+    return render_template('pages/mgb.html', loginRequired=True)
+
+@app.route('/egboost')
+def egboost():
+    return render_template('pages/egboost.html', loginRequired=True)
+
+@app.route('/mgb/tools')
+def tools():
+    return render_template('pages/tools.html', loginRequired=True)
+    
 @app.route('/mgb/profile')
 def profile():
-    return render_template('pages/profile.html', isLandingPage = False)
+    return render_template('pages/profile.html', loginRequired=True)
 
 if __name__ == '__main__':
     app.run(port=1000, debug=True)
